2a47b2a762ec4f953bca2aec239cca0922c32b72
Mmaerro <89846526+Mmaerro@users.noreply.github.com>
2023-02-06 12:29:39 -0300

    ultimos cambios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer";
 import Navbar from "./componentes/Navbar/NavBar";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ItemListContainer prop='Hola Bienvenido' />
     </div>
   );
 }
---
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import "./ItemListContainer.css"
+
+const ItemListContainer = (props) => {
+
+  return (
+    <>
+      <h2>{props.prop}</h2>
+    </>
+  );
+};
+
+export default ItemListContainer;
